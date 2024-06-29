2f9b404c6aa518341b9354e31a8677a8a3823c24
osman efe <oasiiss34@gmail.com>
2024-06-30 02:22:39 +0300

    Create Author Model in posts app

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    sur_name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    email = models.EmailField()
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE) # on_delete=models.CASCADE Yazar Silinirse Yazarın Tüm Postları silinir
