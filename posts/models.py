cf1baf3d724de7ff37464417bd3692efe19b4195
osman efe <oasiiss34@gmail.com>
2024-07-05 01:36:33 +0300

    Models of the posts application have been added to the admin panel

--- posts/models.py
+++ posts/models.py
@@ -9,8 +9,14 @@ class Author(models.Model):
     age = models.PositiveIntegerField()
     email = models.EmailField()
 
+    def __str__(self):
+        return f"{self.name} {self.sur_name}"
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
