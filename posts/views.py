bdd6a3b90a2a5777d83edc0dd50537db97b45944
osman efe <oasiiss34@gmail.com>
2024-07-06 02:34:45 +0300

    Create PostView api

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from posts.models import Post
 
 # Create your views here.
+
+class PostView(APIView):
+    def get(self, request):
+        posts_title = [post.title for post in Post.objects.all()]
+
+        return Response(posts_title)
+
