19e18c578bf992babb756ff957c7d6b60090ed75
osman efe <oasiiss34@gmail.com>
2024-07-13 21:29:14 +0300

    Created viewset

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
-from posts.views import PostView, PostDetailView, TagView, TagDetailView, AuthorView, AuthorDetailView
+from django.urls import path, include
+from posts.views import PostViewSet, AuthorViewSet, TagViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+router.register('authors', AuthorViewSet)
+router.register('tags', TagViewSet)
 
 urlpatterns = [
-    path('', PostView.as_view()),
-    path('<int:pk>/', PostDetailView.as_view()),
-    path('tags/', TagView.as_view()),
-    path('tags/<int:pk>/', TagDetailView.as_view()),
-    path('authors/', AuthorView.as_view()),
-    path('authors/<int:pk>/', AuthorDetailView.as_view())
+    path('', include(router.urls))
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,31 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, viewsets
 from posts.models import Post, Tag, Author
 from posts.serializers import TagSerializer, AuthorSerializer, PostSerializer
 
 # Create your views here.
-class PostView(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-class TagView(generics.ListCreateAPIView):
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer  
-
-class TagDetailView(generics.RetrieveDestroyAPIView):
+class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
-class AuthorView(generics.ListCreateAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
-
-class AuthorDetailView(generics.RetrieveDestroyAPIView):
+class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
