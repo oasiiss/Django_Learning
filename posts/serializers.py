aa0f67ed0a2af8e465a94e9d5bb9b2e0699fdd07
osman efe <oasiiss34@gmail.com>
2024-07-13 03:08:27 +0300

    Created Model Serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,71 +1,19 @@
 from rest_framework import serializers
 from posts.models import Tag, Author, Post
-from django.shortcuts import get_object_or_404
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__" # Tüm Fieldlerın Kullanılacağı anlamına geliyor
 
-class TagSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=50)
-
-    def create(self, validated_data):
-        # return Tag.objects.create(name=validated_data.get("name"))
-        return Tag.objects.create(**validated_data)
-
-
-class AuthorSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
-    sur_name = serializers.CharField(max_length=100)
-    age = serializers.IntegerField()
-    email = serializers.EmailField()
-
-    def create(self, validated_data):
-        return Author.objects.create(**validated_data)
-
-
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=100)
-    content = serializers.CharField()
-    created = serializers.DateTimeField(read_only=True)
-    updated = serializers.DateTimeField(read_only=True)
-    is_published = serializers.BooleanField()
-
-    author = AuthorSerializer(read_only=True)
-    tags = TagSerializer(many=True, required=False, read_only=True)
     
-    author_id = serializers.IntegerField(write_only=True)
-    tag_ids = serializers.ListField(child=serializers.IntegerField(), write_only=True, required=False)
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = "__all__"
 
-    def create(self, validated_data):
-        author_id = validated_data.pop("author_id")
-        tag_ids = validated_data.pop("tag_ids", [])
 
-        author = get_object_or_404(Author, id=author_id)
-        post = Post.objects.create(author=author, **validated_data)
-
-        if tag_ids:
-            tags = Tag.objects.filter(id__in=tag_ids)
-            post.tags.set(tags)
-
-        return post
-    
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.content = validated_data.get("content", instance.content)
-        instance.is_published = validated_data.get("is_published", instance.is_published)
-        
-        author_id = validated_data.get("author_id")
-        
-        if author_id:
-            author = get_object_or_404(Author, id=author_id)
-            instance.author = author
-
-        tag_ids = validated_data.get("tag_ids", [])
-        if tag_ids:
-            instance.tags.clear()
-            tags = Tag.objects.filter(id__in=tag_ids)
-            instance.tags.set(tags)
-        
-        instance.save()
-        return instance
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
